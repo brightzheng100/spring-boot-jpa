1d7e1a58a45fe2fb9fd8a139cafe5fa8bd5d8ef1
Bright Zheng <bright.zheng@outlook.com>
2017-09-18 13:55:56 +0800

    Initial commit

---
+++ src/main/java/hello/DefaultController.java
@@ -0,0 +1,26 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Controller
+ */
+@RestController
+public class DefaultController {
+
+    private static final Logger log = LoggerFactory.getLogger(DefaultController.class);
+
+    @Autowired
+    private CustomerRepository repository;
+
+    @RequestMapping("/")
+    public Iterable<Customer> home() {
+        return repository.findAll();
+    }
+
+}
